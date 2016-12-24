5be387d7e04c736edb0b4715a3e18e27a67a2c51
Sam Mottera <samanthms131211@gmail.com>
2016-12-24 22:12:03 +0530

    Add files via upload

---
+++ Torque_Script.py
@@ -0,0 +1,13 @@
+#EF: end effector
+#AX1..6 : Axes torque
+#EF pan/tilt starts with 1,2
+####################################################
+EF_Weight = 1.500 #Kgrams
+EF_radius = 100 #millimeters
+EF_Force = EF_Weight * 9.8 / 1000 #mass times acceleration
+EF_Torque = EF_Force * EF_radius
+EF_HoldingTorque = EF_Weight * (EF_radius / 10) #kg.cm
+print('EF_Torque',EF_Torque)
+print('EF_Holding Torque', EF_HoldingTorque)
+
+
