5aa506e235512245fa59a0e219a02a8137bf39fe
Sam Mottera <samanthms131211@gmail.com>
2016-12-25 19:50:15 +0530

    Add files via upload

--- Torque_Script.py
+++ Torque_Script.py
@@ -9,5 +9,23 @@ EF_Torque = EF_Force * EF_radius
 EF_HoldingTorque = EF_Weight * (EF_radius / 10) #kg.cm
 print('EF_Torque',EF_Torque)
 print('EF_Holding Torque', EF_HoldingTorque)
-#include all six axes later
 
+print('----------------------------')
+vStepper = 1.00 #angular velocity of the stepper motor shaft rad/sec or rev/sec
+deg_per_sec = vStepper * 360
+vStepperTorque = 9 #Kg .cm
+gear_ratio = 1/20.0000 #worm gear ratio
+arm_radius = 40.00 #mm
+swing = arm_radius * 2.00 * (22.00/7) * vStepper * gear_ratio #angular velocity mm/sec
+print('swing',swing)
+
+#sec per 60 degrees rotation
+speed = (1/vStepper) * (60.0000/360.0000) * (1/gear_ratio)
+print('seconds per 60 degree', speed)
+
+finalTorque = (1/gear_ratio) * vStepperTorque
+print('Torque=', finalTorque,'KG.cm')
+
+microstepping = 1 #or maybe 1/32
+accuracy = microstepping * arm_radius * 1.8 * gear_ratio
+print(accuracy, 'mm per step')
