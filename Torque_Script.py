f756edbf55321da586082a86ce70e2d3c1db224b
Sam Mottera <samanthms131211@gmail.com>
2016-12-29 22:13:58 -0500

    polli

--- Torque_Script.py
+++ Torque_Script.py
@@ -13,9 +13,9 @@ print('EF_Holding Torque', EF_HoldingTorque)
 print('----------------------------')
 vStepper = 3.00 #angular velocity of the stepper motor shaft rad/sec or rev/sec
 deg_per_sec = vStepper * 360
-vStepperTorque = 11#Kg .cm
-gear_ratio = 1/16.0000 #worm gear ratio
-arm_radius = 900.00 #mm
+vStepperTorque = 9#Kg .cm
+gear_ratio = 1/3.0000 #worm gear ratio
+arm_radius = 150.00 #mm
 swing = arm_radius * 2.00 * (22.00/7) * vStepper * gear_ratio #angular velocity mm/sec
 print('swing',swing)
 
