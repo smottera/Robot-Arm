51fa45e648fae070b3764bad545fbf0febb06fa5
Sam Mottera <samanthms131211@gmail.com>
2017-04-19 21:23:35 -0400

    ArduinoDriver
    Dir/Pulse

--- Arduino_Driver.c
+++ Arduino_Driver.c
@@ -1,36 +1,47 @@
 #define DISTANCE 3200
 
-int StepCounter = 0;
-int Stepping = false;
+const int Dir_DS = 2;
+const int Dir_Sh = 3;
+const int Dir_St = 4;
+const int Dir_Rst = 5;
+
+const int Pul_DS = 6;
+const int Pul_Sh = 7;
+const int Pul_St = 8;
+const int Pul_Rst = 9;
 
 void setup() {                
-  pinMode(8, OUTPUT);     
-  pinMode(9, OUTPUT);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-
-  pinMode(3,INPUT);
+ 
+//Direction Reg 
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  
+//Pulse Reg
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);  
+  
+  //Initialize Regs
+  digitalWrite(Dir_Rst, LOW);
+  digitalWrite(Pul_Rst, LOW);
+  
 }
 
 void loop() {
-  if (digitalRead(3) == LOW && Stepping == false)
-  {
-    Stepping = true;
-  }
+    digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+}
 
-  if (Stepping == true)
-  {
-    digitalWrite(9, HIGH);
-    delay(1);          
-    digitalWrite(9, LOW); 
-    delay(1);
+void pump(pul1,pul2,pul3,pul4,pul5,pul6,dir1,dir2,dir3,dir4,dir5,dir6)
+{
+	//Load Serial Direction
 
-    StepCounter = StepCounter + 1;
-
-    if (StepCounter == DISTANCE)
-    {
-      StepCounter = 0;
-      Stepping = false;
-    }
-  }
+	//Load Serial Pulse
+	
+	//Reset Pulse Reg
+	
+	//Reset Direction Reg
 }
