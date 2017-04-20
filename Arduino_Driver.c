3b2619f5d1de4d9f15f4af63edc78d28aa28dc32
Sam Mottera <samanthms131211@gmail.com>
2017-04-19 21:52:18 -0400

    Updated Driver
    Ardu

--- Arduino_Driver.c
+++ Arduino_Driver.c
@@ -25,8 +25,10 @@ void setup() {
   pinMode(9, OUTPUT);  
   
   //Initialize Regs
-  digitalWrite(Dir_Rst, LOW);
-  digitalWrite(Pul_Rst, LOW);
+  digitalWrite(Dir_Rst, HIGH);
+  digitalWrite(Pul_Rst, HIGH);
+  digitalWrite(Dir_Sh, LOW);
+  digitalWrite(Pul_Sh, LOW);
   
 }
 
@@ -35,19 +37,47 @@ void loop() {
   digitalWrite(9, LOW);
 }
 
-void pump(pul1,pul2,pul3,pul4,pul5,pul6,dir1,dir2,dir3,dir4,dir5,dir6)
+
+//Pumps the shift registers with the apt signals
+void pump(unsigned int pul1,unsigned int pul2,unsigned int pul3,unsigned int pul4,unsigned int pul5,unsigned int pul6,
+unsigned int dir1,unsigned int dir2,unsigned int dir3,unsigned int dir4,unsigned int dir5,unsigned int dir6)
 {
 	//Load Serial Direction
+		//Let go of Reset
+		digitalWrite(Dir_Rst, LOW);
+		shift_Dir(dir1);
+		shift_Dir(dir2);
+		shift_Dir(dir3);
+		shift_Dir(dir4);
+		shift_Dir(dir5);
+		shift_Dir(dir6);
+		//!!!Shift!!!
+		digitalWrite(Dir_Sh,HIGH);
+		digitalWrite(Dir_Sh,LOW);
 		
-
 	//Load Serial Pulse
+		//Let go of Reset
+		digitalWrite(Dir_Rst, LOW);
+		shift_Pul(Pul1);
+		shift_Pul(Pul2);
+		shift_Pul(Pul3);
+		shift_Pul(Pul4);
+		shift_Pul(Pul5);
+		shift_Pul(Pul6);
+		//!!!Store!!!
+		digitalWrite(Pul_Sh,HIGH);
+		digitalWrite(Pul_Sh,LOW);
 	
 	//Reset Pulse Reg
+	digitalWrite(Pul_Rst, HIGH);
 	
 	//Reset Direction Reg
+	digitalWrite(Dir_Rst, HIGH);
 }
 
-void shift_Dir(signal)
+
+//Writes to SR
+void shift_Dir(unsigned int signal)
 {
 		if(signal)
 				{
@@ -63,6 +93,26 @@ void shift_Dir(signal)
 				//Just Shift
 				digitalWrite(Dir_Sh, HIGH);
 				digitalWrite(Dir_Sh, LOW);
+		}
+}
+
+//Writes to SH
+void shift_Pul(unsigned int signal)
+{
+		if(signal)
+				{
+					//Set DS to HIGH
+					digitalWrite(Pul_DS, HIGH);
+					digitalWrite(Pul_DS, LOW);
+					//Shift
+					digitalWrite(Pul_Sh, HIGH);
+					digitalWrite(Pul_Sh, LOW);
+				}
+		else
+		{
+				//Just Shift
+				digitalWrite(Pul_Sh, HIGH);
+				digitalWrite(Pul_Sh, LOW);
 		}
 }
 
